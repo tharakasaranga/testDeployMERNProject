aea6d5e0b81ccbad35cd301de2d9cf7ea20da038
K.A. Tharaka Saranga <152644410+tharakasaranga@users.noreply.github.com>
2025-07-14 23:36:47 +0530

    Update CrudApp.jsx

--- src/components/CrudApp.jsx
+++ src/components/CrudApp.jsx
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-const API_URL = 'https://replit.com/@tharakasaranga7/testDeployMERNProjectbackEnd-1/api/items'
+const API_URL = 'https://testdeploymernprojectbackend-production.up.railway.app/api/items'
 //https://testdeploymernprojectbackend-production.up.railway.app
+//https://replit.com/@tharakasaranga7/testDeployMERNProjectbackEnd-1
 
 function CrudApp() {
   const [items, setItems] = useState([])
